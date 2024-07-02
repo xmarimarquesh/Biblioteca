b2cc9a287cdb9fb8b80c259e8ffe4eab77b14e24
Mari Marques <140663136+xmarimarquesh@users.noreply.github.com>
2024-07-01 22:01:19 -0300

    Add files via upload

---
+++ Livro.java
@@ -0,0 +1,13 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ID;
+
+    public Livro(){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ID = ID;
+    }
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Livro> livros = new LinkedList<>();
+    }
+}
